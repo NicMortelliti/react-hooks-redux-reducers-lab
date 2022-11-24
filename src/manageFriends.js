667df28c1612b4c6f82a749dd8fc24a81e4cc939
Nic Mortelliti <npmortelliti@gmail.com>
2022-11-24 13:17:56 -0800

    Assignment complete

--- src/manageFriends.js
+++ src/manageFriends.js
@@ -1,3 +1,17 @@
+const state = { friends: [] };
+
 export function manageFriends(state, action) {
-  // your code here
+  switch (action.type) {
+    case "friends/add":
+      let friend = action.payload;
+      return { friends: [...state.friends, friend] };
+
+    case "friends/remove":
+      return {
+        friends: state.friends.filter((friend) => friend.id !== action.payload),
+      };
+
+    default:
+      return state;
+  }
 }
